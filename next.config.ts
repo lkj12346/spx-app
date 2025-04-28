d3dc6dea0e11b36f34c7e97000e3b06da49b3af7
이 명재 <lkj12346@MSE.com>
2025-04-28 16:34:21 +0900

    fix: next.config.ts redeclare error 완전 수정

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 });
 
 module.exports = nextConfig;
